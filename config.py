044c0c3f00b11f156363f092b9573ae46fcd830f
GhOZs <108602616+GhOZs@users.noreply.github.com>
2022-07-11 02:54:38 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+BOT_TOKEN = os.environ.get('5489944774:AAERFAjy5I-2N-KgKbIf_XNGu0okEz50RW8','')
+TG_API_ID = os.environ.get('16346841',0)
+TG_API_HASH = os.environ.get('41994f821a9a2e16195eea854e18bdbc','')
+TG_ADMIN_USER = os.environ.get('script1722','')
+ROOT_DIR = 'root'
